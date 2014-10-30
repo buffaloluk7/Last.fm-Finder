0fd61077e2669657c8df013d925ccc7637ac412d
Lukas Streiter <lukas@Lukass-MacBook-Pro.local>
2014-10-30 15:25:49 +0100

    Initial Commit

---
+++ Last.fm Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Last.fm Finder
+//
+//  Created by Lukas Streiter on 30/10/14.
+//  Copyright (c) 2014 Lukas Streiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
